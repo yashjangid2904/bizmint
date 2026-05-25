f8302bdf8f6089afef156f2760da9538c8d6377c
Khushi Jangid <lawdocsteam22@outlook.com>
2026-05-25 11:59:54 +0530

    remove two sections

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,12 @@
 
 import React from "react";
 import HeroSection from "@/components/sections/HeroSection";
-import Process from "@/components/sections/Process";
-import WhyChooseUs from "@/components/sections/WhyChooseUs";
 import FAQ from "@/components/sections/FAQ";
 
 export default function Home() {
   return (
     <div className="relative bg-white dark:bg-black">
       <HeroSection />
-      <Process />
-      <WhyChooseUs />
       <FAQ />
     </div>
   );
